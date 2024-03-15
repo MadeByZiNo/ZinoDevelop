b83532c87082f670f31aa7b9f1ef86d618336da2
MadeByZiNo <38098073+MadeByZiNo@users.noreply.github.com>
2024-03-15 15:28:32 +0900

    프로젝트 초기 업로드

---
+++ zinoDevelop/src/main/java/com/zinoSoft/zinoDevelop/ZinoDevelopApplication.java
@@ -0,0 +1,13 @@
+package com.zinoSoft.zinoDevelop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZinoDevelopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZinoDevelopApplication.class, args);
+	}
+
+}
